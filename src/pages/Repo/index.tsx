cd5fc308bbcf3e62096ac18f004e58dc449d34a1
Henrique Cunha <henrique.cunha@corpus.inf.br>
2021-03-14 12:25:54 -0300

    feat: add Repo Component

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,9 +1,51 @@
 import React from 'react'
 
-// import { Container } from './styles';
+import {
+  Container,
+  Breadcrumb,
+  RepoIcon,
+  Stats,
+  StarIcon,
+  ForkIcon,
+  LinkButton,
+  GithubIcon,
+} from './styles'
+import { Link } from 'react-router-dom'
 
 const Repo: React.FC = () => {
-  return <div />
+  return (
+    <Container>
+      <Breadcrumb>
+        <RepoIcon />
+        <Link className={'username'} to={'/henriquequeirozcunha'}>
+          henriquequeirozcunha
+        </Link>
+        <span>/</span>
+        <Link className={'reponame'} to={'/henriquequeirozcunha/clone-github'}>
+          clone-github
+        </Link>
+      </Breadcrumb>
+      <p>Contains all of clone-github code!</p>
+
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>0</b>
+          <span>stars</span>
+        </li>
+        <li>
+          <ForkIcon />
+          <b>10</b>
+          <span>forks</span>
+        </li>
+      </Stats>
+
+      <LinkButton href='https://github.com/henriquequeirozcunha/clone-github'>
+        <GithubIcon />
+        <span>View on GitHub</span>
+      </LinkButton>
+    </Container>
+  )
 }
 
 export default Repo
