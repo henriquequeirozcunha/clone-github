e7f9a41424eadbc2dcc5c1b1d40cf81b07ce2c42
Henrique Cunha <henrique.cunha@corpus.inf.br>
2021-03-14 10:19:33 -0300

    feat: add Profile component

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,9 +1,46 @@
 import React from 'react'
-
-// import { Container } from './styles';
+import { Container, Main, LeftSide, RigthSide, Repos } from './styles'
+import ProfileData from '../../components/ProfileData'
+import RepoCard from '../../components/RepoCard'
 
 const Profile: React.FC = () => {
-  return <div />
+  return (
+    <Container>
+      <Main>
+        <LeftSide>
+          <ProfileData
+            name={'Henrique Cunha'}
+            username={'henriquequeirozcunha'}
+            avatarUrl={'https://avatars.githubusercontent.com/u/52020128?v=4'}
+            followers={800}
+            following={1200}
+            company={'Company'}
+            location={'Brazil'}
+            email={'henriquequeirozcunha@hotmail.com'}
+            blog={undefined}
+          />
+        </LeftSide>
+        <RigthSide>
+          <Repos>
+            <h2>Random Repos</h2>
+            <div>
+              {[1, 2, 3, 4, 5, 6].map((n) => (
+                <RepoCard
+                  key={n}
+                  username={'henriquequeirozcunha'}
+                  reponame={'github-conte'}
+                  description={'Contains github clone repository'}
+                  language={n % 3 === 0 ? 'Javascript' : 'Typescript'}
+                  stars={8}
+                  forks={2}
+                />
+              ))}
+            </div>
+          </Repos>
+        </RigthSide>
+      </Main>
+    </Container>
+  )
 }
 
 export default Profile
