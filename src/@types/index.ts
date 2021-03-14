128617ed00b6b8491d8cf9bac60ee43c298b9cc2
Henrique Cunha <henrique.cunha@corpus.inf.br>
2021-03-14 12:41:48 -0300

    refactor: make Profile page get dynamic data from github API

---
+++ src/@types/index.ts
@@ -0,0 +1,24 @@
+export type APIUser = {
+  login: string
+  name: string
+  followers: number
+  following: number
+  public_repos: number
+  avatar_url: string
+  blog?: string
+  company?: string
+  email?: string
+  location?: string
+}
+
+export type APIRepo = {
+  name: string
+  owner: {
+    login: string
+  }
+  stargazers_count: number
+  forks: number
+  html_url: string
+  language?: string
+  description?: string
+}
